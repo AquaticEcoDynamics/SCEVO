95a165ff84b5d5d24230f2843183dc36b8160061
Giles Knight <62376886+gilesnknight@users.noreply.github.com>
2022-02-10 10:42:17 +0800

    dependency fixes

--- dashboard/R/app_ui.R
+++ dashboard/R/app_ui.R
@@ -2,7 +2,7 @@
 #' 
 #' @param request Internal parameter for `{shiny}`. 
 #'     DO NOT REMOVE.
-#' @import shiny leaflet scevo
+#' @import shiny leaflet 
 #' @noRd
 app_ui <- function(request) {
   tagList(
--- dashboard/tests/testthat.R
+++ dashboard/tests/testthat.R
@@ -1,4 +1,3 @@
 library(testthat)
-library(secvo)
 
-test_check("secvo")
+
